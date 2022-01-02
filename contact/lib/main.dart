63fe2be0ae8c53fd5544545c06c22c0978657356
JYKil <kilga2402@gmail.com>
2022-01-02 11:44:53 +0900

    Scaffold

--- contact/lib/main.dart
+++ contact/lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(const MyApp());
 }
-/*
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
   @override
@@ -12,19 +13,37 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       //home: Text('Hello')
       //home: Icon(Icons.star)
-      home: Image.asset('icon.png')
-      */
-/*
-      home: Center(
-          child: Container(width: 50, height: 50, color: Colors.red)
+      //home: Image.asset('icon.png')
+      //home: Center(
+      //    child: Container(width: 50, height: 50, color: Colors.red)
+      //)
+      home: Scaffold(
+          appBar: AppBar(
+              title: Text('kilga''s app'),
+              centerTitle: false,
+          ),
+          body: Container(
+              child: Text('this is Body')
+          ),
+          bottomNavigationBar: BottomAppBar(
+              child: Container(
+                  height: 60,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Icon(Icons.phone),
+                      Icon(Icons.message),
+                      Icon(Icons.contact_page),
+                    ],
+                  )
+              )
+          )
       )
-      *//*
-
     );
   }
 }
-*/
 
+/*
 // Default src provided by Google
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
@@ -79,20 +98,11 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Kilga Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'kilga Flutter'),
     );
   }
 }
-
+*/
 
